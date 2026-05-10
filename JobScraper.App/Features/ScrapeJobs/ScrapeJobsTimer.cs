f18a846ef9c840692eba768ce767655ca1fc3cd8
Michael Addleton <michael@addleton.dev>
2026-05-10 11:36:29 +0100

    feature: Add not implemented timer method and nuget package

--- JobScraper.App/Features/ScrapeJobs/ScrapeJobsTimer.cs
+++ JobScraper.App/Features/ScrapeJobs/ScrapeJobsTimer.cs
@@ -1,6 +1,11 @@
-﻿namespace JobScraper.App.Features.ScrapeJobs;
+﻿using Microsoft.Azure.Functions.Worker;
+
+namespace JobScraper.App.Features.ScrapeJobs;
 
 public class ScrapeJobsTimer
 {
-    
+    public static void RunTimer([TimerTrigger("0 0 */4 * * *")] TimerInfo timerInfo, FunctionContext context)
+    {
+        throw new NotImplementedException();
+    }
 }
