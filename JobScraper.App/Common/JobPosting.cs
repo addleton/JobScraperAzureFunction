2bba0cd3f5e2d451e0539f1ec42a67374ff9ccaa
Michael Addleton <michael@addleton.dev>
2026-05-10 11:17:29 +0100

    chore: Add properties to JobPosting

--- JobScraper.App/Common/JobPosting.cs
+++ JobScraper.App/Common/JobPosting.cs
@@ -1,6 +1,14 @@
-﻿namespace JobScraper.App.Common;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobScraper.App.Common;
 
 public class JobPosting
 {
-    
+    public int Id { get; init; }
+    [Required] [MaxLength(100)] public required string Title { get; init; }
+    [Required] [MaxLength(10000)] public required string Description { get; init; }
+    [Required] [MaxLength(100)] public required string Company { get; init; }
+    [Required] [MaxLength(1000)] public required string Url { get; init; }
+    public int Salary { get; init; }
+    public bool EmailSent { get; init; }
 }
