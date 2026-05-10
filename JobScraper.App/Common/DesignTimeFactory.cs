335a271de0c7b8a89a82bd27518bb264d7009b5d
Michael Addleton <michael@addleton.dev>
2026-05-10 11:18:12 +0100

    chore: Add design time factory

--- JobScraper.App/Common/DesignTimeFactory.cs
+++ JobScraper.App/Common/DesignTimeFactory.cs
@@ -1,6 +1,15 @@
-﻿namespace JobScraper.App.Common;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
-public class DesignTimeFactory
+namespace JobScraper.App.Common;
+
+public class DesignTimeFactory : IDesignTimeDbContextFactory<JobDbContext>
 {
-    
+    public JobDbContext CreateDbContext(string[] args)
+    {
+        var builder = new DbContextOptionsBuilder<JobDbContext>();
+        var connectionString = "Server=(localdb)\\mssqllocaldb;Database=JobScraperLocal;Trusted_Connection=True;MultipleActiveResultSets=true";
+        builder.UseSqlServer(connectionString);
+        return new JobDbContext(builder.Options);
+    }
 }
