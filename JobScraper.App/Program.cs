d6d2b9f2eeeb69769dd5633adfb03ed71b63a49c
Michael Addleton <michael@addleton.dev>
2026-05-11 18:32:15 +0100

    chore: Add classes to DI

--- JobScraper.App/Program.cs
+++ JobScraper.App/Program.cs
@@ -1,6 +1,10 @@
-using JobScraper.App.Features.ScrapeJobs;
+using JobScraper.App.Common;
+using JobScraper.App.Features.Repository;
+using JobScraper.App.Features.ScrapeReedJobs;
+using JobScraper.App.Features.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +16,16 @@ builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights()
     .AddHttpClient()
-    .AddSingleton<JobBoardsClient>();
+    .AddDbContext<JobDbContext>(options =>
+    {
+        string connectionString = Environment.GetEnvironmentVariable("SqlConnectionString")
+                                  ??
+                                  "Server=(localdb)\\mssqllocaldb;Database=JobScraperLocal;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        options.UseSqlServer(connectionString);
+    })
+    .AddSingleton<ReedApiClient>()
+    .AddSingleton<JobFilterService>()
+    .AddScoped<JobRepository>();
 
 builder.Build().Run();
