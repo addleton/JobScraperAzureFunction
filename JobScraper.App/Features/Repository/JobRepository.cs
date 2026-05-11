5fad07c15bccb6e4bb7eabeecc341d5e35dc0e8e
Michael Addleton <michael@addleton.dev>
2026-05-11 18:31:19 +0100

    feature: Add JobRepository class for database specific methods

---
+++ JobScraper.App/Features/Repository/JobRepository.cs
@@ -0,0 +1,44 @@
+﻿using JobScraper.App.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobScraper.App.Features.Repository;
+
+public class JobRepository
+{
+    private readonly JobDbContext _context;
+
+    public JobRepository(JobDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveJobsAsync(List<JobPosting> jobs)
+    {
+        List<string> newIdList = jobs.Select(x => x.JobId).ToList();
+        List<string> newSources = jobs.Select(x => x.Source).Distinct().ToList();
+
+        var existingCompositeList = await _context.JobPostings
+            .Where(j => newIdList.Contains(j.JobId) && newSources.Contains(j.Source))
+            .Select(j => new { j.JobId, j.Source })
+            .ToListAsync();
+
+        List<JobPosting> newJobs = jobs.Where(newJob =>
+            !existingCompositeList.Any(existing =>
+                existing.Source == newJob.Source && existing.JobId == newJob.JobId)
+        ).ToList();
+
+        if (newJobs.Count > 0)
+        {
+            await _context.JobPostings.AddRangeAsync(newJobs);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<List<JobPosting>> GetAllJobsAsync()
+    {
+        return await _context.JobPostings.ToListAsync();
+    }
+}
