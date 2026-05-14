7186567c8addb02ae97a44c41e24d488ef3b98e9
Michael Addleton <michael@addleton.dev>
2026-05-14 14:29:18 +0100

    feature: Add and implement SendEmailTimer

--- JobScraper.App/Features/SendEmail/SendEmailTimer.cs
+++ JobScraper.App/Features/SendEmail/SendEmailTimer.cs
@@ -1,6 +1,39 @@
-﻿namespace JobScraper.App.Features.SendEmail;
+﻿using JobScraper.App.Common;
+using JobScraper.App.Features.Repository;
+using Microsoft.Azure.Functions.Worker;
+
+namespace JobScraper.App.Features.SendEmail;
 
 public class SendEmailTimer
 {
-    
+    private readonly EmailBuilder _builder;
+    private readonly JobRepository _repo;
+    private readonly EmailSender _sender;
+
+    public SendEmailTimer(JobRepository repo, EmailBuilder builder, EmailSender sender)
+    {
+        _repo = repo;
+        _builder = builder;
+        _sender = sender;
+    }
+
+    [Function(nameof(SendEmailTimer))]
+    public async Task RunTimer([TimerTrigger("0 0 6/24 * * *", RunOnStartup = true)] TimerInfo timerInfo,
+        FunctionContext context)
+    {
+        List<JobPosting> jobPostings = await _repo.GetAllJobsWithoutEmailSentAsync();
+
+        if (jobPostings.Count < 1) return;
+
+        string email = _builder.BuildJobEmailHtml(jobPostings);
+
+        bool success = await _sender.SendEmailAsync(email);
+
+        if (success)
+        {
+            List<int> jobIds = jobPostings.Select(x => x.Id).ToList();
+
+            await _repo.SetEmailSentTrueAsync(jobIds);
+        }
+    }
 }
